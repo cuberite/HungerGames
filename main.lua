77fdd274d1832f1c0d92220414e3d23707c49dfc
fankins <mc@jwow.ru>
2014-01-05 07:39:10 -0800

    Commenting out some lines;
    Commenting out some lines;

--- main.lua
+++ main.lua
@@ -1,79 +1,76 @@
 
--- Global variables
 PLUGIN = {};
-tp = {};	-- Reference to own plugin object
+tp = {};
 ItemsIDToChest = {};
 PlayerNameIG={};
 
-ItemsIDToChest[1] = 258
-ItemsIDToChest[2] = "260,260,260"
-ItemsIDToChest[3] = 261
+
+-- Items that will spawn in the chests on the map
+ItemsIDToChest[1] = 258;
+ItemsIDToChest[2] = "260,260,260";
+ItemsIDToChest[3] = 261;
 ItemsIDToChest[4] = "262,262,262,262,262,262,262,262";
-ItemsIDToChest[5] = "266,266,266,266"
-ItemsIDToChest[6] = 267
-ItemsIDToChest[7] = 265
-ItemsIDToChest[8] = 270
-ItemsIDToChest[9] = 271
-ItemsIDToChest[10] = 272
-ItemsIDToChest[11] = 273
-ItemsIDToChest[12] = 274
-ItemsIDToChest[13] = 275
-ItemsIDToChest[14] = 283
-ItemsIDToChest[15] = 284
-ItemsIDToChest[16] = 285
-ItemsIDToChest[17] = 286
-ItemsIDToChest[18] = "296,296,296,296,296,296"
-ItemsIDToChest[19] = "297,297"
-ItemsIDToChest[20] = 298
-ItemsIDToChest[21] = 299
-ItemsIDToChest[22] = 300
-ItemsIDToChest[23] = 301
-ItemsIDToChest[24] = 302
-ItemsIDToChest[25] = 303
-ItemsIDToChest[26] = 304
-ItemsIDToChest[27] = 305
-ItemsIDToChest[28] = 306
-ItemsIDToChest[29] = 307
-ItemsIDToChest[30] = 308
-ItemsIDToChest[31] = 309
+ItemsIDToChest[5] = "266,266,266,266";
+ItemsIDToChest[6] = 267;
+ItemsIDToChest[7] = 265;
+ItemsIDToChest[8] = 270;
+ItemsIDToChest[9] = 271;
+ItemsIDToChest[10] = 272;
+ItemsIDToChest[11] = 273;
+ItemsIDToChest[12] = 274;
+ItemsIDToChest[13] = 275;
+ItemsIDToChest[14] = 283;
+ItemsIDToChest[15] = 284;
+ItemsIDToChest[16] = 285;
+ItemsIDToChest[17] = 286;
+ItemsIDToChest[18] = "296,296,296,296,296,296";
+ItemsIDToChest[19] = "297,297";
+ItemsIDToChest[20] = 298;
+ItemsIDToChest[21] = 299;
+ItemsIDToChest[22] = 300;
+ItemsIDToChest[23] = 301;
+ItemsIDToChest[24] = 302;
+ItemsIDToChest[25] = 303;
+ItemsIDToChest[26] = 304;
+ItemsIDToChest[27] = 305;
+ItemsIDToChest[28] = 306;
+ItemsIDToChest[29] = 307;
+ItemsIDToChest[30] = 308;
+ItemsIDToChest[31] = 309;
 
-ItemsIDToChest[32] = 314
-ItemsIDToChest[33] = 315
-ItemsIDToChest[34] = 316
-ItemsIDToChest[35] = 317
+ItemsIDToChest[32] = 314;
+ItemsIDToChest[33] = 315;
+ItemsIDToChest[34] = 316;
+ItemsIDToChest[35] = 317;
+ItemsIDToChest[36] = 319;
+ItemsIDToChest[37] = 320;
+ItemsIDToChest[38] = 322;
+ItemsIDToChest[39] = 349;
+ItemsIDToChest[40] = 350;
 
-ItemsIDToChest[36] = 319
-ItemsIDToChest[37] = 320
-ItemsIDToChest[38] = 322
-
-ItemsIDToChest[39] = 349
-ItemsIDToChest[40] = 350
-
-ItemsIDToChest[41] = 360
-ItemsIDToChest[42] = 363
-ItemsIDToChest[43] = 364
-ItemsIDToChest[44] = 365
-ItemsIDToChest[45] = 366
-ItemsIDToChest[46] = 367
+ItemsIDToChest[41] = 360;
+ItemsIDToChest[42] = 363;
+ItemsIDToChest[43] = 364;
+ItemsIDToChest[44] = 365;
+ItemsIDToChest[45] = 366;
+ItemsIDToChest[46] = 367;
 
 
-
-
-ItemsIDToChest[47] = 391
-ItemsIDToChest[48] = 392
-ItemsIDToChest[49] = 393
-ItemsIDToChest[50] = 305
-ItemsIDToChest[51] = 0
-ItemsIDToChest[52] = 0
-ItemsIDToChest[53] = 0
-ItemsIDToChest[54] = 0
-ItemsIDToChest[55] = 0
-ItemsIDToChest[56] = 0
-ItemsIDToChest[57] = 0
-ItemsIDToChest[58] = 0
-ItemsIDToChest[59] = 0
-ItemsIDToChest[60] = 0
-ItemsIDToChest[61] = 0
+ItemsIDToChest[47] = 391;
+ItemsIDToChest[48] = 392;
+ItemsIDToChest[49] = 393;
+ItemsIDToChest[50] = 305;
+ItemsIDToChest[51] = 0;
+ItemsIDToChest[52] = 0;
+ItemsIDToChest[53] = 0;
+ItemsIDToChest[54] = 0;
+ItemsIDToChest[55] = 0;
+ItemsIDToChest[56] = 0;
+ItemsIDToChest[57] = 0;
+ItemsIDToChest[58] = 0;
+ItemsIDToChest[59] = 0;
+ItemsIDToChest[60] = 0;
+ItemsIDToChest[61] = 0;
 
 
 
@@ -84,32 +81,24 @@ ItemsIDToChest[65] = "365,365,365"
 ItemsIDToChest[66] = "366,366,367"
 ItemsIDToChest[67] = "367,367,368"
 
-PlayerId = {};
+PlayerId = {}; 
 
-
--- AllItemsIDToChest = "256,257,260,261,262,266,265,267,268,269,270,271,272,273,274,275,282,283,284,285,286,288,290,291,292,294,296,297,298,299,300,301,302,303,304,305,306,307,308,309,314,315,316,317,319,320,322,334,349,350,357,360,363,364,365,366,367,391,392,393,394,395"
-
-
-PlayersTotal = 0
+PlayersTotal = 0;
 --------------------------
 
 WinnerName = "unknown"
-
-canbreak = false
-canplace = false
-
+canbreak = false;
+canplace = false;
 CanMoving = false;
-BeforeTime = false
-EdtitorMode = false
-ThisisWin = false
-
-JustLoad = false
+BeforeTime = false;
+EdtitorMode = false;
+ThisisWin = false;
+JustLoad = false;
 
 function Initialize(Plugin)
 	PLUGIN = Plugin
 	Plugin:SetName("HungerGamesPlugin")
 	Plugin:SetVersion("4")
-
 	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_LEFT_CLICK,    OnPlayerLeftClick)
 	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_PLACING_BLOCK, OnPlayerPlacingBlock)
 	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_PLACED_BLOCK,  OnPlayerPlacedBlock)
@@ -124,20 +113,17 @@ function Initialize(Plugin)
 	cPluginManager.AddHook(cPluginManager.HOOK_HANDSHAKE,            OnHandshake)
 
 	PM = cRoot:Get():GetPluginManager()
-	PM:BindCommand("/revi",         "hungergames.Reviv",   reviveblockstwo,      " reviveblocks");
-	PM:BindCommand("/canbreak",     "hungergames.Break",   CanBreak,             " CanBreak blocks");
-	PM:BindCommand("/canplace",     "hungergames.Place",   CanPlace,             " CanPlace blocks");
-	PM:BindCommand("/restartround", "hungergames.restart", RestartRound,         " RestartRound");
-	PM:BindCommand("/startround",   "hungergames.start",   startRound,           " StartRound")
+	PM:BindCommand("/revi",         "hungergames.Reviv",   reviveblockstwo,      " Restore foliage destroyed during the round");
+	PM:BindCommand("/canbreak",     "hungergames.Break",   CanBreak,             " Allow to break blocks");
+	PM:BindCommand("/canplace",     "hungergames.Place",   CanPlace,             " Allow to place blocks");
+	PM:BindCommand("/restartround", "hungergames.restart", RestartRound,         " Restart Round");
+	PM:BindCommand("/startround",   "hungergames.start",   startRound,           " Start Round")
 	PM:BindCommand("/players",      "ShowPlayersInGame",   ShowPlayersInGame,    " Show Players in Hunger Game");
-	-- PM:BindCommand("/pl",        "ShowPlayersInGame",   pl,                   " Show Players in Hunger Game");
 	PM:BindCommand("/em",           "EdtitorModeActivate", EdtitorModeActivate,  " 0 or 1 to enable / disable chest editor mode");
-
-	LoadSettings()
-	LoadTimes()
-	UnLoadItemsIntoBD()
-	RestartRound()
-
+	LoadSettings() -- Load spawn position
+	LoadTimes() -- Load timers from Config.ini
+	UnLoadItemsIntoBD() -- Clean the contents of the chests 
+	RestartRound() -- Restart round, then reset all the counters of time to return to the pre-spawn all, if the required number of players had accumulated (2), teleport all players on the map.
 	return true
 end;
 
