c5c2a5a4714e57ae8055c775f8a7af7abfeaa08f
STRWarrior <niels.breuker@hotmail.nl>
2014-04-19 23:08:58 +0200

    Finished the Info.lua file documentation Added readme generated by InfoDump

--- Info.lua
+++ Info.lua
@@ -10,7 +10,13 @@ g_PluginInfo =
 {
 	Name = "HungerGames",
 	Date = "2014-4-17",
-	Description = "TODO!",
+	Description = 
+[[
+A hunger games plugin for MCServer.
+
+You are able to create multiple different arenas in different worlds. People can join those arenas and battle each other.
+The players search for chests where they can find loot. They need it so for survival. Eventualy only one player is left and he'll be the winner.
+]],
 	
 	Commands =
 	{
@@ -98,6 +104,61 @@ g_PluginInfo =
 					Handler = HandleLeaveCommand,
 				},
 			},
+		},
+	},
+	
+	AditionalInfo =
+	{
+		{
+			Title = "Setting up an arena",
+			Contents =
+[[
+Assuming the server owner/admin already has a map.
+
+1) {%%b}Create the arena{%%/b}
+   To create an arena you use "/hg create [ArenaName]". 
+   The coordinates where you stand will be the coordinates for the lobby. When a player joins or dies in an arena he will be teleported there.
+   
+2) Setting the bounding box
+   Then you need to set the bounding box of the arena. 
+   All the chests within this bounding box will be refilled once the arena starts so it is needed. You set it using "/hg bb (point|1,2)". 
+   So basicly point 1 is on one side of the arena and point 2 on the opposite.
+   
+3) Adding spawnpoints
+   Then you have to add spawnpoints. You add them by using "/hg add"
+   When an arena starts all the players will be sent to a different spawnpoint.
+]],
+		},
+		
+		{
+			Title = "Configuring HungerGames",
+			Contents =
+[[
+First open the "Config.cfg" file.
+
+I think the variables CountDownTime, NoDamageTime, PreventMonsterSpawn and PreventAnimalSpawn speak for themselves, but just to be sure:
+  CountDownTime: The time a player can't move and do anything when an arena starts.
+  NoDamageTime: The amount of time a player can't take any damage from another player
+  PreventMonsterSpawn: If set to true then monsters (zombies, skeletons creepers etc) can't spawn inside the bounding box of an arena.
+  PreventAnimalSpawn: Same as PreventMonsterSpawn but then for animals (pigs, cows, chicken etc).
+  
+The hardest part is configuring the loot what a chest can have, but once you know it it's really easy.
+To add an item to the list of available loot you go to ChestContent and add this behind it: 
+{%%code}
+{
+	ItemType = ItemID,
+	ItemMeta = ItemMeta,
+	Chance = Chance,
+	Enchantment = "enchantmentname",
+},
+{%%/code}
+ItemType: The itemid you want to have for example 267 for a diamond sword.
+ItemMeta: The meta you want to give the item.
+Enchantment: You don't have to put this in, but if you want an enchantment you simply give the name of the enchantment with an equal symbol and the level. For example: "sharpness=2" If you want multiple enchantments you use a ";" between the enchantments.
+Chance: This is important. -- IF this item gets chosen then the server chooses a random number between 1 and the given number and if it's 1 this item gets put in the chest. The problem with this is the more items you have available the less chance this item gets chosen.
+
+Also don't forget the comma. You have to place them at the exact spot as where they are in this example.
+]]
 		},
 	},
 };
---
+++ README.md
@@ -0,0 +1,27 @@
+A hunger games plugin for MCServer.
+
+You are able to create multiple different arenas in different worlds. People can join those arenas and battle each other. The players search for chests where they can find loot. They need it so for survival. Eventualy only one player is left and he'll be the winner. 
+
+# Commands
+
+### General
+| Command | Permission | Description | 
+| ------- | ---------- | ----------- | 
+|/hg add | hungergames.add | Adds a spawnpoint for the selected arena.| 
+|/hg create | hungergames.create | Creates an new arena.| 
+|/hg join | hungergames.join | Join an arena| 
+|/hg leave | hungergames.leave | Leave an arena| 
+|/hg select | hungergames.select | Used to select an arena. In the selected arena you can add spawnpoints.| 
+|/hg setboundingbox | hungergames.setsize | Sets the size of the arena. All the chests inside will be filled.| 
+
+
+
+# Permissions
+| Permissions | Description | Commands | Recommended groups |
+| ----------- | ----------- | -------- | ------------------ |
+hungergames.add |  | `/hg add` | 
+hungergames.create |  | `/hg create` | 
+hungergames.join |  | `/hg join` | 
+hungergames.leave |  | `/hg leave` | 
+hungergames.select |  | `/hg select` | 
+hungergames.setsize |  | `/hg setboundingbox` | 
